4a08c0024f9b26ec9b6632e075e399fc5fec9e8c
Фаль Татьяна <tatty.fal@gmail.com>
2026-04-23 15:05:20 +0600

    Добавила проверку на повторения и отсортировала массив

--- src/App.tsx
+++ src/App.tsx
@@ -16,30 +16,38 @@ const App = () => {
   ]);
 
   const changeNumbers = () => {
-    setNumbers([
-      { num: Math.floor(Math.random() * 37) },
-      { num: Math.floor(Math.random() * 37) },
-      { num: Math.floor(Math.random() * 37) },
-      { num: Math.floor(Math.random() * 37) },
-      { num: Math.floor(Math.random() * 37) }
-    ]
-    );
+    const newNumbers: Circle[] = [];
+
+    while (newNumbers.length < 5) {
+      const random = Math.floor(Math.random() * 36) + 1;
+
+      if (!newNumbers.some(n => n.num === random)) {
+        newNumbers.push({ num: random });
+      }
+    }
+
+    newNumbers.sort((a, b) => a.num - b.num);
+
+    setNumbers(newNumbers);
   };
 
   return (
-    <>
-      <button onClick={changeNumbers}>Get new numbers</button>
-      <Circle num={numbers[0].num}>
-      </Circle>
-      <Circle num={numbers[1].num}>
-      </Circle>
-      <Circle num={numbers[2].num}>
-      </Circle>
-      <Circle num={numbers[3].num}>
-      </Circle>
-      <Circle num={numbers[4].num}>
-      </Circle>
-    </>
+    <div className="wrapper">
+
+      <button className="btn" onClick={changeNumbers}>Get new numbers</button>
+      <div className="numbers">
+        <Circle num={numbers[0].num}>
+        </Circle>
+        <Circle num={numbers[1].num}>
+        </Circle>
+        <Circle num={numbers[2].num}>
+        </Circle>
+        <Circle num={numbers[3].num}>
+        </Circle>
+        <Circle num={numbers[4].num}>
+        </Circle>
+      </div>
+    </div>
   )
 };
 
